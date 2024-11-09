6a6a9da54643f6d93f5188e0ec0592f54d2edd19
AnnE <Krossingsqsq@gmail.com>
2024-11-09 13:51:16 +0300

    add Book model, BookDAO, BookController

---
+++ src/main/java/org/spring/learn/controllers/BookController.java
@@ -0,0 +1,10 @@
+package org.spring.learn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,8 @@
 <html>
 <body>
-<h2>Hello World!</h2>
-<a href="/people">home</a>
+<br>
+<a href="/people">people</a>
+<hr>
+<a href="/books">books</a>
 </body>
 </html>
